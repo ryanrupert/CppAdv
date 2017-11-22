0b905384cb9eeff4827faa7ef3f0e4ad42e0ab57
Ryan Rupert <mudge25+code@gmail.com>
2017-11-22 10:51:24 -0800

    Add test case for Prime number function

--- test.cpp
+++ test.cpp
@@ -15,3 +15,18 @@ TEST_CASE("Time converter")
 	CHECK(date.year == 1);
 	CHECK(date.day == 352);
 }
+
+TEST_CASE("Prime number")
+{
+	CHECK(adv::isPrime(10) == false);
+	CHECK(adv::isPrime(2) == true);
+	CHECK(adv::isPrime(11) == true);
+	CHECK(adv::isPrime(43) == true);
+	CHECK(adv::isPrime(97) == true);
+	CHECK(adv::isPrime(99) == false);
+	CHECK(adv::isPrime(12) == false);
+	CHECK(adv::isPrime(44) == false);
+	CHECK(adv::isPrime(1000) == false);
+	CHECK(adv::isPrime(1187) == true);
+	CHECK(adv::isPrime(991) == true);
+}
