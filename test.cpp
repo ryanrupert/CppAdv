6bb25bcb77ccb931e5df72749226db57c12750dd
Ryan Rupert <mudge25+code@gmail.com>
2017-11-28 21:26:28 -0800

    Add more test case checks

--- test.cpp
+++ test.cpp
@@ -38,4 +38,8 @@ TEST_CASE("Round decimal")
 	CHECK(adv::round(53.625, 2) == 53.63);
 	CHECK(adv::round(100.5265, 3) == 100.527);
 	CHECK(adv::round(526.78526, 4) == 526.7853);
+	CHECK(adv::round(2.365896, 4) == 2.3659);
+	CHECK(adv::round(52.157956, 5) == 52.15796);
+	CHECK(adv::round(865.9573156, 6) == 865.957316);
+	CHECK(adv::round(3.00000003, 3) == 3.000);
 }
